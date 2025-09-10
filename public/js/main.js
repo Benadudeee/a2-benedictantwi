8f6e8b6ccb2716fb1d4c9eb81bce03985a840a8d
Benedict A <sbenedict637@gmail.com>
2025-09-10 18:32:52 -0400

    >:)

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,91 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
+const loadData = async function ( ){
+    const response = await fetch("/get_list", {
+      method: "GET"
+    })
+
+    if(!response.ok){
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const taskItems = await response.text();
+    const taskItemsData = JSON.parse(taskItems);
+
+    // Load Cards
+    const cardContainer = document.querySelector(".cardlist");
+    cardContainer.innerHTML = null;
+
+    taskItemsData.forEach( (item, i) => {
+        const cardItem = document.createElement('div');
+        cardItem.className = `card--item ${ (!item.is_finished)? 'incomplete' : 'complete' }`;
+
+        // Make Due / Created a date object as date objects in mode server are strings.
+        const created_at = new Date(item.created_at);
+        const due_at_date = new Date(item.due_at);
+
+        // Html for cards, will code the buttons outside the function after it loads
+        cardItem.innerHTML =   ` 
+            
+              <h3> ${item.name} </h3>
+              <p> ${item.description} </p>
+
+              <p>${(!item.is_finished)? 'incomplete' : 'complete'} </p>
+
+              <p> Due in: ${item.time_till_finished} days</p>
+              <p> Due at: ${due_at_date.toDateString()} </p>
+              <p> Created at: ${created_at.toDateString()} </p>
+
+              <div class="card--item--actions ${i}"> 
+                <button data-id="${item.id}" class="button delete"> Delete Task </button>
+                <button data-id="${item.id}" class="button finish"> ${ (!item.is_finished)? 'Finish Task' : 'Set Incomplete' } </button>
+              </div>
+           
+      `;
+      cardContainer.appendChild(cardItem);
+    }
+  );
+
+
+  hydrateCardBtns();
+}
+
+const hydrateCardBtns = function () {
+  const deleteBtns = document.querySelectorAll(".button.delete");
+  const finishBtns = document.querySelectorAll(".button.finish");
+
+  console.log(deleteBtns, finishBtns);
+
+  deleteBtns.forEach((btn) => btn.addEventListener("click" , () => removeTask(btn.dataset.id)));
+  finishBtns.forEach((btn) => btn.addEventListener("click", () => toggleFinished(btn.dataset.id)));
+}
+
+const removeTask = async function ( id ) {
+  const url = `/delete_task/${id}`;
+
+  console.log(url);
+  const response = await fetch(url, {
+    method: 'DELETE'
+  });
+
+  const status = await response.text();
+  if(status === 'success'){
+    loadData();
+  }
+}
+
+const toggleFinished = async function ( id ) {
+  const url = `/toggle_finished/${id}`;
+  const response = await fetch(url, {
+    method: 'PUT'
+  });
+
+  const status = await response.text();
+  if(status === 'success'){
+    console.log("success")
+    loadData();
+  }
+}
+
 
 const submit = async function( event ) {
   // stop form submission from trying to load
@@ -7,21 +94,44 @@ const submit = async function( event ) {
   // remains to this day
   event.preventDefault()
   
-  const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  const name = document.querySelector(".name");
+  const description = document.querySelector(".description");
+  const due_at = document.querySelector(".date");
+  const status = document.querySelector(".status");
 
+  console.log(due_at.value);
+  
+  const json = { 
+    id: null,
+    name: name.value ,
+    description: description.value,
+    due_at: new Date(due_at.value),
+    status: status.value,
+
+    is_finished: false,
+    created_at: new Date(),
+    time_till_finished: null,
+  };
+
+  const body = JSON.stringify( json );
+  
   const response = await fetch( "/submit", {
     method:"POST",
     body 
   })
 
-  const text = await response.text()
+  if(response.ok){
+    const data = await response.text();
+    // console.log(response);
+    // console.log( "text:", data )
 
-  console.log( "text:", text )
+    loadData();
+  }
 }
 
 window.onload = function() {
-   const button = document.querySelector("button");
+  const button = document.querySelector(".form-add-btn");
   button.onclick = submit;
+
+  loadData();
 }
--- server.improved.js
+++ server.improved.js
@@ -8,17 +8,62 @@ const http = require( "http" ),
       dir  = "public/",
       port = 3000
 
+/**
+ * Planner
+ * 
+ * name: name of task
+ * description: description of task
+ * 
+ * created_at: current date it was made
+ * due_at: date where it's due
+ * 
+ * time_till_done: (created_at - due_at)
+ * is_finished: boolean when the task is due
+ * status: anything you want "finished" if (is_finished) is true
+ */
 const appdata = [
-  { "model": "toyota", "year": 1999, "mpg": 23 },
-  { "model": "honda", "year": 2004, "mpg": 30 },
-  { "model": "ford", "year": 1987, "mpg": 14} 
+  // Dummy  task
+  {
+    id: "id1203020",
+    name: "Get started on this app",
+    description: "Try to make a single task by clicking create task",
+    due_at: new Date('2099-08-24'),
+    status: "working",
+
+    is_finished: false,
+    created_at: new Date(),
+
+    time_till_finished: 27011,
+  }
 ]
+
+const generateAppdataId = function ( ) {
+    return `id${Math.random().toString(16).slice(2)}`
+}
+
+const lookupAppdataId = function( id ){
+  let ind = -1;
+
+  for(let i = 0; i < appdata.length; i++){
+    ind = i;
+
+    if(id === appdata[i].id){
+      return ind;
+    }
+  }
+
+  return ind;
+}
 
 const server = http.createServer( function( request,response ) {
   if( request.method === "GET" ) {
     handleGet( request, response )    
   }else if( request.method === "POST" ){
     handlePost( request, response ) 
+  }else if( request.method === "DELETE"){
+    handleDelete( request, response )
+  } else if(request.method === "PUT"){
+    handlePut( request, response )
   }
 })
 
@@ -26,9 +71,71 @@ const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
 
   if( request.url === "/" ) {
-    sendFile( response, "public/index.html" )
+    sendFile( response, "public/index.html" );
+
+  }else if(request.url === "/get_list"){
+    const data = appdata;
+
+    request.on("data", function() {
+
+    })
+    request.on("end", function() {
+      response.writeHead( 200, "OK", {"Content-Type" : "text/plain"} );
+      response.end(JSON.stringify(appdata));
+    })
+
   }else{
     sendFile( response, filename )
+  }
+}
+
+const handlePut = function (request, response) {
+  const urlParams = request.url.split("/").slice(1);
+  // console.log(urlParams)
+  if(urlParams[0] === 'toggle_finished'){
+    request.on("data", function(){
+    });
+  
+    request.on("end", function(){
+      //Parse out i data
+      const index = lookupAppdataId(urlParams[1]); // convert param into string to properly splice
+      
+      if(index == -1){
+        response.writeHead( 404, "NOTFOUND", {"Content-Type": "text/plain" } );
+        response.end("id can't be found")
+      }
+
+      appdata[index].is_finished = !(appdata[index].is_finished);
+
+      // console.log(appdata[index].is_finished);
+
+      response.writeHead( 200, "OK", {"Content-Type": "text/plain" } );
+      response.end("success")
+    });
+  }
+}
+
+const handleDelete = function ( request, response ) {
+
+  // Gets the main url with the paramters main/${id} this
+  // is also used in the update function to update if the
+  // task is finished of not. The main url is index [0]
+  const urlParams = request.url.split("/").slice(1);
+  // console.log(urlParams);
+
+  if(urlParams[0] === 'delete_task'){
+    request.on("data", function(){
+    });
+  
+    request.on("end", function(){
+      //Parse out i data
+      const index = lookupAppdataId(urlParams[1]); // convert param into string to properly splice
+
+      appdata.splice(index, 1);
+
+      response.writeHead( 200, "OK", {"Content-Type": "text/plain" } );
+      response.end("success")
+    });
   }
 }
 
@@ -40,12 +147,20 @@ const handlePost = function( request, response ) {
   })
 
   request.on( "end", function() {
-    console.log( JSON.parse( dataString ) )
+    const postData = JSON.parse(dataString);
+    // ... do something with the data here!!
 
-    // ... do something with the data here!!!
+    postData.id = generateAppdataId();
+
+    const time_till_finished = new Date(postData.due_at) - new Date(postData.created_at);
+    postData.time_till_finished = Math.round(time_till_finished / (1000 * 60 * 60 * 24)); // Daytime conversion
+
+    // Convert time_till_finished in days
+
+    appdata.push(postData);
 
     response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
-    response.end("test")
+    response.end( "JSON.parse(dataString)" );
   })
 }
 
